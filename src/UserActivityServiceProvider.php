08340f0b8805b04c6e5ec2a693829868cc72a0e8
varunazad <54257710+varunazad@users.noreply.github.com>
2025-07-30 18:59:30 +0530

    Create UserActivityServiceProvider.php

---
+++ src/UserActivityServiceProvider.php
@@ -0,0 +1,22 @@
+namespace Varunazad\UserActivityLog;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserActivityServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/user-activity-log.php' => config_path('user-activity-log.php'),
+        ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/user-activity-log.php', 'user-activity-log'
+        );
+    }
+}
